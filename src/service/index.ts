3662c75b2a6aff47dfe714ae8f52545943589680
清水 <2756848729@qq.com>
2023-07-05 13:19:33 +0800

    feat: 请求轮播图数据

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,22 +1,24 @@
+import { BASE_URL, TIME_OUT } from './config'
 import HCCRequest from './request'
 
 // 创建封装后的请求实例
 const hccRequest = new HCCRequest({
-  baseURL: 'http://xxx',
+  baseURL: BASE_URL,
+  timeout: TIME_OUT,
   interceptors: {
     requestSuccess(config) {
-      console.log('请求成功拦截')
+      console.log('实例拦截器-请求成功拦截')
       return config
     },
     requestFailure(err) {
-      console.log('请求失败拦截')
+      console.log('实例拦截器-请求失败拦截')
     },
     responseSuccess(res) {
-      console.log('响应成功拦截')
+      console.log('实例拦截器-响应成功拦截')
       return res
     },
     responseFailure(err) {
-      console.log('响应失败拦截')
+      console.log('实例拦截器-响应失败拦截')
     }
   }
 })
