fe775ed64ea762a9cfa508e4ac6e4cba71f4f655
清水 <2756848729@qq.com>
2023-07-05 12:40:49 +0800

    fix: 配置名称

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,3 +1,5 @@
 // baseUrl timeOut 等配置
-export const BASE_URL = 'http://'
-export const TIME_OUNT = 10000
+export const BASE_URL = 'http://codercba.com:9002'
+export const TIME_OUT = 10000
+
+// 区分开发环境 生产环境
