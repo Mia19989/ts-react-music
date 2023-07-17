fbc29f4b1a2accd2aee45be74ebe3b42944cd103
清水 <2756848729@qq.com>
2023-07-17 09:47:44 +0800

    feat: 轮播图的上下图切换

--- src/views/discover/child-views/recommend/child-cpns/top-banner/index.tsx
+++ src/views/discover/child-views/recommend/child-cpns/top-banner/index.tsx
@@ -1,15 +1,18 @@
-import { useAppSelector } from '@/store/hook'
-import React, { memo } from 'react'
-import type { ReactNode } from 'react'
+import React, { memo, useRef } from 'react'
+import type { ReactNode, ElementRef } from 'react'
 import { shallowEqual } from 'react-redux'
 import { Carousel } from 'antd'
 import { BannerControl, BannerLeft, BannerRight, BannerWrapper } from './style'
+import { useAppSelector } from '@/store/hook'
 
 interface IProps {
   children?: ReactNode
 }
 
 const TopBanner: React.FC<IProps> = () => {
+  // 创建 ref 对象，注意类型约束，初始值设置 null
+  const bannerRef = useRef<ElementRef<typeof Carousel>>(null)
+
   // 获取 state 中 banner 数据
   const { banners } = useAppSelector(
     (state) => ({
@@ -18,11 +21,22 @@ const TopBanner: React.FC<IProps> = () => {
     shallowEqual
   )
 
+  /** 切换上一张轮播图 */
+  const handlePrevClick = () => {
+    // ref 属性用于访问组件实例，调用组件上的方法或获取组件上的属性
+    bannerRef?.current?.prev()
+  }
+
+  /** 切换下一章轮播图 */
+  const handleNextClick = () => {
+    bannerRef?.current?.next()
+  }
+
   return (
     <BannerWrapper>
       <div className="banner wrap-v2">
         <BannerLeft>
-          <Carousel autoplay>
+          <Carousel autoplay ref={bannerRef}>
             {banners.map((item) => (
               <div key={item.imageUrl} className="banner-item">
                 <img
@@ -37,8 +51,8 @@ const TopBanner: React.FC<IProps> = () => {
         <BannerRight></BannerRight>
 
         <BannerControl>
-          <button className="btn left" />
-          <button className="btn right" />
+          <button className="btn left" onClick={handlePrevClick} />
+          <button className="btn right" onClick={handleNextClick} />
         </BannerControl>
       </div>
     </BannerWrapper>
